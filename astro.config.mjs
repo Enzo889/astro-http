ab1e404cecdb28ef119ec03b0a0807cfd7b3b414
Enzo Bustamante <enzobustamante895@gmail.com>
2026-04-01 16:20:29 -0300

    feat: add likes counter components  and vue integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vue from '@astrojs/vue';
 
 // import node from "@astrojs/node";
 import cloudflare from "@astrojs/cloudflare";
@@ -10,7 +11,7 @@ import db from "@astrojs/db";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), db()],
+  integrations: [mdx(), sitemap(), db(), vue()],
   output: "hybrid",
   adapter: cloudflare()
 });
